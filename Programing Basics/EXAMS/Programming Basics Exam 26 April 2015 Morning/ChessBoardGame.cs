f0f73ed6441d88b6839f963b6bba4827bf20f206
Димитър Желязков <do0msday.bkp@gmail.com>
2015-06-20 05:47:27 +0300

    Exams

---
+++ Programing Basics/EXAMS/Programming Basics Exam 26 April 2015 Morning/ChessBoardGame.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ChessBoardGame
+{
+    class ChessBoardGame
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()), W = 0, B = 0;
+            char[] strArray = Console.ReadLine().ToCharArray();
+            for (int i = 0; i < strArray.Length; i++)
+            {
+                if (i >= n * n) { break; }
+                if (i % 2 == 0 && char.IsUpper(strArray[i])) { W += strArray[i]; }
+                else if(i % 2 != 0 && char.IsUpper(strArray[i])) { B+= strArray[i]; }
+                else if(i % 2 == 0 && char.IsLetterOrDigit(strArray[i])) { B+= strArray[i]; }
+                else if(i%2 != 0 && char.IsLetterOrDigit(strArray[i])) {W+=strArray[i];}
+            }
+            if (B == W) { Console.WriteLine("Equal result: {0}", W); }
+            else
+            {
+                Console.WriteLine("The winner is: {0} team", W > B ? "white" : "black");
+                Console.WriteLine(Math.Abs(B-W));
+            }
+        }
+    }
+}
