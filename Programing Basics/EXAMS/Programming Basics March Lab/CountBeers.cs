45486794810b61a69a303046634a641fe3cff97c
Димитър Желязков <do0msday.bkp@gmail.com>
2015-06-20 18:22:21 +0300

    Exams

---
+++ Programing Basics/EXAMS/Programming Basics March Lab/CountBeers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CountBeers
+{
+    class CountBeers
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int stacks = 0, beers = 0;
+            while (input != "End")
+            {
+                string[] words = input.Split(' ');
+                if (words[1] == "stacks") { stacks += int.Parse(words[0]); }
+                else if (words[1] == "beers") { beers += int.Parse(words[0]); }
+                input = Console.ReadLine();
+            }
+            if (beers % 20 > 0 && beers >= 20) { stacks += beers / 20; beers = beers % 20; }
+            Console.WriteLine("{0} stacks + {1} beers", stacks, beers);
+            
+        }
+    }
+}
