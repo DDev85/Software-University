a1a42b71f852c419cd2864eb86c3867d5cb3a6b6
Димитър Желязков <do0msday.bkp@gmail.com>
2015-06-20 06:37:05 +0300

    Exams

---
+++ Programing Basics/EXAMS/Programming Basics Exam 26 April 2015 Morning/KingOfThieves.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace KingOfThieves
+{
+    class KingOfThieves
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[] g = Console.ReadLine().ToCharArray();
+
+           
+            char[] array = new string('-',n).ToCharArray();
+            array[n / 2] = g[0];
+            Console.WriteLine(array);
+            for (int i = 1; i <= n/2; i++)
+            {
+                array[(n / 2) - i] = g[0];
+                array[(n / 2) + i] = g[0];
+                Console.WriteLine(array);
+            }
+            for (int i = 0; i <= n / 2 - 1; i++)
+            {
+                array[i] = '-';
+                array[n - i - 1 ] = '-';
+                Console.WriteLine(array);
+            }
+
+        }
+    }
+}
