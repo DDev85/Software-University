5381694a5edff3f041874fd4213400d8f6421a96
Димитър Желязков <do0msday.bkp@gmail.com>
2015-06-21 09:08:01 +0300

    Codes

---
+++ Programing Basics/4. Console Input Output Homework/FibonacciNumbers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FibonacciNumbers
+{
+    class FibonacciNumbers
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number: ");
+            int input = int.Parse(Console.ReadLine());
+            for (int i = 0; i < input; i++)
+            {
+                Console.Write("{0} ", Fibonacci(i));
+            }
+            Console.WriteLine();
+        }
+        private static int Fibonacci(int n)
+        {
+            int a = 0, b = 1;
+            for (int i = 0; i < n; i++)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+            return a;
+        }
+    }
+}
---
+++ Programing Basics/4. Console Input Output Homework/SumOfnNumbers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SumOfnNumbers
+{
+    class SumOfnNumbers
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number: ");
+            int numbers = int.Parse(Console.ReadLine());
+            double num = 0;
+            for (int i = 0; i < numbers; i++)
+            {
+                num += double.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Sum is: " + num);
+        }
+    }
+}
