8c8a2e31142945ae77f6e66b81d1bd91cbfd5102
Димитър Желязков <do0msday.bkp@gmail.com>
2015-06-21 09:49:28 +0300

    Codes

---
+++ Programing Basics/4. Console Input Output Homework/NumbersDividableFive.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumbersDividableFive
+{
+    class NumbersDividableFive
+    {
+        static void Main(string[] args)
+        {
+            int a, b, count = 0;
+            Console.Write("Enter start number: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Enter end number: ");
+            b = int.Parse(Console.ReadLine());
+            for (int i = a; i <= b; i++)
+            {
+                if (i % 5 == 0) { count++; }
+            }
+            Console.WriteLine(count + " numbers devidet by 5 !");
+        }
+    }
+}
